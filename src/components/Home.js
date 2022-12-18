31f377020058744a92024af6d68a6e71217e85c0
Sowmith Ganduri <90488562+Sowwybaka@users.noreply.github.com>
2022-12-18 20:53:04 +0000

    added animation

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,20 @@
 import React from 'react'
+import '../App.css';
+
 
 function Home() {
     return (
 <>
-<div className="container">
-    <div className="row">
-        <div className="col">
-            <img src="https://avatars.githubusercontent.com/u/90488562?v=4" alt="Profile Picture" />
+<div className="container py-5">
+    <div className="row ">
+        <div className="col text-center">
+            <img id='profile' className='rounded-circle' src="https://avatars.githubusercontent.com/u/90488562?v=4" alt="Profile Picture" />
         </div>
-        <div className="col">
+        <div className="col d-flex align-items-center">
+            <div>
             <h1>I am Sowmith</h1>
             <h3>7th grade student living in California. I am currently learning web development.</h3>
+            </div>
         </div>
     </div>
 </div>
